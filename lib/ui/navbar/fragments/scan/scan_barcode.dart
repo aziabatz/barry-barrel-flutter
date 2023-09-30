1bd7b6cc145fc7b6dccd9285f1b48e952f74b9cf
Ahmed Ziabat <aziabatz@alumnos.unex.es>
2023-09-30 17:20:39 +0200

    Improved Barcode scanning

--- lib/ui/navbar/fragments/scan/scan_barcode.dart
+++ lib/ui/navbar/fragments/scan/scan_barcode.dart
@@ -1,6 +1,6 @@
+import 'package:bbarr/ui/navbar/fragments/scan/qr_overlay.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter_qr_bar_scanner/qr_bar_scanner_camera.dart';
 
 class ScanBarcode extends StatefulWidget {
   const ScanBarcode({super.key});
@@ -10,77 +10,69 @@ class ScanBarcode extends StatefulWidget {
 }
 
 class _ScanBarcodeState extends State<ScanBarcode> {
-String _scanBarcode = 'Unknown';
 
-Future<void> startBarcodeScanStream() async {
+  String? _qrInfo = 'Scan a QR/Bar code';
+  bool _camState = false;
 
-    FlutterBarcodeScanner.getBarcodeStreamReceiver(
-            '#ff6666', 'Cancel', true, ScanMode.BARCODE)!
-        .listen((barcode) => print(barcode));
-  }
-
-Future<void> scanQR() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.QR);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+  _qrCallback(String? code) {
     setState(() {
-      _scanBarcode = barcodeScanRes;
+      _camState = false;
+      _qrInfo = code;
     });
   }
 
-// Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> scanBarcodeNormal() async {
-    String barcodeScanRes;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
-      print(barcodeScanRes);
-    } on PlatformException {
-      barcodeScanRes = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
+  _scanCode() {
     setState(() {
-      _scanBarcode = barcodeScanRes;
+      _camState = true;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-        alignment: Alignment.center,
-        child: Flex(
-            direction: Axis.vertical,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                  onPressed: () => scanBarcodeNormal(),
-                  child: Text('Start barcode scan')),
-              ElevatedButton(
-                  onPressed: () => scanQR(), child: Text('Start QR scan')),
-              ElevatedButton(
-                  onPressed: () => startBarcodeScanStream(),
-                  child: Text('Start barcode scan stream')),
-              Text('Scan result : $_scanBarcode\n',
-                  style: TextStyle(fontSize: 20))
-            ]));
+  void initState() {
+    super.initState();
+    _scanCode();
   }
-  
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return _camState
+          ? Center(
+        child: SizedBox(
+          height: 1000,
+          width: 500,
+          child: QRBarScannerCamera(
+            onError: (context, error) => Text(
+              error.toString(),
+              style: TextStyle(color: Colors.red),
+            ),
+            qrCodeCallback: (code) {
+              _qrCallback(code);
+            },
+            child: Positioned.fill(
+            child: Container(
+              decoration: ShapeDecoration(
+                shape: QrScannerOverlayShape(
+                  borderColor: Colors.white,
+                  borderRadius: 10,
+                  borderLength: 10,
+                  borderWidth: 5,
+                  cutOutSize: 250,
+                ),
+              ),
+            ),
+          )
+          ),
+        ),
+      )
+          : Center(
+        child: Text(_qrInfo!),
+      );
+  }
 }
